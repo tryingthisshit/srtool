5fabaf0b39858dd26cfdf1da39dc2b7f196d056d
Maciek Albin <madmck@gmail.com>
2012-11-29 18:56:51 +0000

    SSA renaming

--- src/srt/tool/SSAVisitor.java
+++ src/srt/tool/SSAVisitor.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import srt.ast.AssignStmt;
 import srt.ast.Decl;
 import srt.ast.DeclRef;
+import srt.ast.Expr;
 import srt.ast.visitor.impl.DefaultVisitor;
 
 public class SSAVisitor extends DefaultVisitor {
@@ -16,26 +17,40 @@ public class SSAVisitor extends DefaultVisitor {
 	}
 	
 	private String getSSAName(String name) {
-		return null;
+		if(!index.containsKey(name)){
+			index.put(name,0);
+		}
+		return String.format("%s$%d", name, index.get(name));
 	}
 	
 	private void incrementSSAIndex(String name) {
-		
+		index.put(name, index.get(name)+1);
 	}
 	
 	@Override
 	public Object visit(Decl decl) {
-		return super.visit(decl);
+		Decl ndecl = new Decl(getSSAName(decl.getName()), decl.getType(), decl);
+		return super.visit(ndecl);
 	}
 	
 	@Override
 	public Object visit(DeclRef declRef) {
-		return super.visit(declRef);
+		DeclRef ndeclRef = new DeclRef(getSSAName(declRef.getName()), declRef);
+		return super.visit(ndeclRef);
 	}
 	
 	@Override
 	public Object visit(AssignStmt assignment) {
-		return super.visit(assignment);
+		Expr rhs = assignment.getRhs();
+		Expr parsedExpr = (Expr) super.visit(rhs);
+
+		DeclRef d = assignment.getLhs();
+		incrementSSAIndex(d.getName());
+		DeclRef nd = (DeclRef) this.visit(d);
+		
+		AssignStmt newAssign = new AssignStmt(nd, parsedExpr, assignment);
+
+		return newAssign;
 	}
 
 }
